d4e98761b55fa77df7c972b368b715c0f85a777c
Diogo Areas <dareas@stone.com.br>
2017-08-29 15:54:40 -0300

    Add project files.

---
+++ AuthStudy/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AuthStudy.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Logar(string userName, string password)
+        {
+            var user = userName;
+            var pass = password;
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ AuthStudy/Filters/AuthorizationFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AuthStudy.Filters
+{
+    public class AuthorizationFilter : IAuthorizationFilter
+    {
+       public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var ctx = context;
+        }
+    }
+}
